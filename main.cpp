c8b1120da237697ac2ea79f3f2e5622997cf58b6
Gleeber <gleeb101@gmail.com>
2019-02-22 04:24:18 -0900

    Fixes to timing of when board updates are called.

--- main.cpp
+++ main.cpp
@@ -12,21 +12,36 @@ int main()
     int turn = 0;
     while(board1.isOpen() && board2.isOpen())
     {
-        board1.update();
-        board2.update();
+        if (turn == 0)
+        {
+            board1.update();
+            board2.update();
+        }
         if (board1.shipsPlaced() && board2.shipsPlaced() && turn == 0) turn = 1;
 
         if (turn == 1)
         {
+            board2.update();
             sf::Vector2i coords = board1.makeMove();
             bool hit = board2.isHit(coords);
-            if(board1.moveMade(coords,hit)) turn = 2;
+            if(board1.moveMade(coords,hit))
+            {
+                board1.update();
+                board2.update();
+                turn = 2;
+            }
         }
         else if (turn == 2)
         {
+            board1.update();
             sf::Vector2i coords = board2.makeMove();
             bool hit = board1.isHit(coords);
-            if(board2.moveMade(coords,hit)) turn = 1;
+            if(board2.moveMade(coords,hit))
+            {
+                board1.update();
+                board2.update();
+                turn = 1;
+            }
         }
     }
 
