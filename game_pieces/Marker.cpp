50ac39ac94d7ce6fd8b24f4f9f1c5d4bd4141e88
RobinTheSprite <madscientistak@gmail.com>
2019-02-21 19:26:33 -0900

    Add Marker class

---
+++ game_pieces/Marker.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mark on 2/21/2019.
+//
+
+#include "Marker.h"
---
+++ game_pieces/Marker.h
@@ -0,0 +1,18 @@
+//
+// Created by Mark on 2/21/2019.
+//
+
+#ifndef CS372_BATTLESHIP_GAME_MARKER_H
+#define CS372_BATTLESHIP_GAME_MARKER_H
+
+
+class Marker
+{
+public:
+    Marker(bool);
+
+private:
+};
+
+
+#endif //CS372_BATTLESHIP_GAME_MARKER_H
