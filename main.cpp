6f21a6d382d5bdcd7d05b3a96f676f718140f43c
Gleeber <gleeb101@gmail.com>
2019-02-21 23:58:45 -0900

    Game starts when both boards lock their ships.

--- main.cpp
+++ main.cpp
@@ -9,10 +9,23 @@ int main()
 {
     GUIBoard board1;
     GUIBoard board2;
+    int turn = 0;
     while(board1.isOpen() && board2.isOpen())
     {
         board1.update();
         board2.update();
+        if (board1.shipsPlaced() && board2.shipsPlaced() && turn == 0) turn = 1;
+
+        if (turn == 1)
+        {
+            // turn 1 stuff
+            turn = 2;
+        }
+        else if (turn == 2)
+        {
+            // turn 2 stuff
+            turn = 1;
+        }
     }
 
     return 0;
